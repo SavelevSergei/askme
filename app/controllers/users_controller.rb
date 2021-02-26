58256abfef0d1dfec9d0e862a090c28121078e84
Sergei_Savelev <lavrentiy83@gmail.com>
2021-02-26 04:24:23 +0300

    add styls and count for questins

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,35 @@
 class UsersController < ApplicationController
+  def index
+    @users = [
+      User.new(
+        id: 1,
+        name: 'Sergei',
+        username: 'SkiZ',
+        avatar_url: 'https://99px.ru/sstorage/1/2020/10/image_12010201411197140419.gif'
+      ),
+      User.new(id: 2, name: 'Misha', username: 'aristofun')
+    ]
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
   def show
-    @hello = 'Привет народ!!!'
+    @user = User.new(
+      name: 'Sergei',
+      username: 'SkiZ',
+      avatar_url: 'https://99px.ru/sstorage/1/2020/10/image_12010201411197140419.gif'
+    )
+
+    @questions = [
+      Question.new(text: 'Как дела?', created_at: Date.parse('25.02.2021')),
+      Question.new(text: 'Как дальше жить?', created_at: Date.parse('25.02.2021'))
+    ]
+
+    @questions_count = @questions.count
+    @new_question = Question.new
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+  def user_avatar(user)
+    if user.avatar_url.present?
+      user.avatar_url
+    else
+      asset_path 'avatar.jpg'
+    end
+  end
+
+  def declination(num, one, two, three)
+    last_digits = num % 10
+
+    return three if (11..14).include?(num % 100)
+
+    return one if last_digits == 1
+
+    return two if last_digits.between?(2, 4)
+
+    three
+  end
 end
