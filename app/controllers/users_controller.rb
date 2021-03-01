9b4fc31921c5c9c71ff466811b0380605986939b
Sergei_Savelev <lavrentiy83@gmail.com>
2021-03-02 02:04:51 +0300

    add sessions controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,35 +1,60 @@
 class UsersController < ApplicationController
+
+  before_action :load_user, except: [:index, :new, :create]
+
+  before_action :authorize_user, except: [:index, :new, :create, :show]
+
   def index
-    @users = [
-      User.new(
-        id: 1,
-        name: 'Sergei',
-        username: 'SkiZ',
-        avatar_url: 'https://99px.ru/sstorage/1/2020/10/image_12010201411197140419.gif'
-      ),
-      User.new(id: 2, name: 'Misha', username: 'aristofun')
-    ]
+    @users = User.all
   end
 
   def new
+    redirect_to root_url, alert: 'Вы уже залогинены' if current_user.present?
+    @user = User.new
+  end
+
+  def create
+    redirect_to root_url, alert: 'Вы уже залогинены' if current_user.present?
+
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to root_url, notice: 'Пользователь успешно зарегистрирован!'
+    else
+      render 'new'
+    end
   end
 
   def edit
   end
 
+  def update
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: 'Данные обновлены'
+    else
+      render 'edit'
+    end
+  end
+
   def show
-    @user = User.new(
-      name: 'Sergei',
-      username: 'SkiZ',
-      avatar_url: 'https://99px.ru/sstorage/1/2020/10/image_12010201411197140419.gif'
-    )
+    @questions = @user.questions.order(created_at: :desc)
 
-    @questions = [
-      Question.new(text: 'Как дела?', created_at: Date.parse('25.02.2021')),
-      Question.new(text: 'Как дальше жить?', created_at: Date.parse('25.02.2021'))
-    ]
+    @new_question = @user.questions.build
 
-    @questions_count = @questions.count
-    @new_question = Question.new
+    @questions_count = @questions.all.count
+  end
+
+  private
+
+  def authorize_user
+    reject_user unless @user == current_user
+  end
+
+  def load_user
+    @user ||= User.find params[:id]
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :name, :username, :avatar_url)
   end
 end
