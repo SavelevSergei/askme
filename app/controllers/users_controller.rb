79ef5df77b81223cbda9fffa23fa79c4fdab0614
Sergei_Savelev <lavrentiy83@gmail.com>
2021-03-16 05:57:42 +0300

    Revert "Revert "add deleting user""
    This reverts commit 588ffcbe93f32c88d3ee44c994e49fdd596031fc.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,6 +37,11 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    @user.destroy
+    redirect_to root_url, notice: 'Пользователь удалён!!!'
+  end
+
   def show
     @questions = @user.questions.order(created_at: :desc)
 
